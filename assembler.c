a325e709b4b1483f3f900d418d605486be97c754
‘AramK0’ <aramk6827@gmail.com>
2025-09-26 23:05:12 +0300

    getting better

--- assembler.c
+++ assembler.c
@@ -14,88 +14,110 @@
 #define TOKEN_SIZE 1024
 
 
-enum{
-    OP_BR = 0, // Branch
-    OP_ADD, //add  1 = 0001
-    OP_LD, // load
-    OP_ST, //store
-    OP_JSR, // jump register
-    OP_AND, //bitwise AND
-    OP_LDR, // load register
-    OP_STR, // store register
-    OP_RTI, // unused
-    OP_NOT, // bitwise NOT
-    OP_LDI, // load indirect 1010 = 10
-    OP_STI, // store indirect
-    OP_JMP, // jump
-    OP_RES, // reserved (unused)
-    OP_LEA, // load effective addresss
-    OP_TRAP // execute trap
-};
-
-
-
 
 int main(){
 
     char **tokenizer = malloc(sizeof(char *) * TOKEN_SIZE);
+    char **string = malloc(TOKEN_SIZE * sizeof(char *));
     char *token;
     char buffer[1024];
     int i = 0;
     uint16_t orig;
     uint16_t ADD;
     uint16_t HALT;
+    uint16_t r0;
+    uint16_t r1;
     
 
+
     FILE *file = fopen("test.s", "r");
+    FILE *f2 = fopen("yes.obj", "wb");
 
     size_t read = fread(buffer, 1, sizeof(buffer) - 1, file);
     buffer[read] = '\0';
 
     token = strtok(buffer, ",\t\n");
-    while(token){
+    for(int e = 0; token; e++){
         tokenizer[i++] = token;
         token = strtok(NULL, " ,\t\n");
+        if(strcmp(tokenizer[e], ".STRINGZ") == 0){
+            token = strtok(NULL, "\n");
+            string[0] = token;
+        }
+        
     }
-    
-    printf("%s\n", tokenizer[0]);
-    printf("%s\n", tokenizer[1]);
-    printf("%s\n", tokenizer[2]);
-    printf("%s\n", tokenizer[3]);
-    printf("%s\n", tokenizer[4]);
-    printf("%s\n", tokenizer[5]);
+    int size = sizeof(string[0]);
+    char text[size];
 
+    
+
+    strcpy(text, string[0]);
+
+   printf("%s\n", text);
+ 
+    
 
     tokenizer[i] = NULL;
-  
-    if(strcmp(tokenizer[0], ".ORIG x3000") == 0){
+    for(int j = 0; j < i; j++){
+        if(strcmp(tokenizer[j], ".ORIG x3000") == 0){
         orig = 0x3000;
         orig = (orig << 8) | (orig >> 8);
-    }
-    if(strcmp(tokenizer[1], "ADD") == 0){
+        fwrite(&orig, sizeof(uint16_t), 1, f2);
+
+        }
+        else if(strcmp(tokenizer[j], "LEA") == 0){
+            uint16_t LOAD_E = 0xE002;
+            //uint16_t pc_offset = malloc(sizeof(uint16_t));
+            
+
+            
+            LOAD_E = (LOAD_E << 8) | (LOAD_E >> 8);
+            fwrite(&LOAD_E, sizeof(uint16_t), 1, f2);
+
+            
+
+
+        }
+        else if(strcmp(tokenizer[j], ".STRINGZ") == 0){
+            //printf("%s\n", tokenizer[j+1]);
+        }
+        else if(strcmp(tokenizer[j], "ADD") == 0){
+            ADD = 0x1007;
+            ADD = (ADD << 8) | (ADD >> 8);
+            fwrite(&ADD, sizeof(uint16_t), 1, f2);
+        }
+        else if(strcmp(tokenizer[j], "PUTS") == 0){
+            uint16_t PUTS = 0xF022;
+            PUTS = (PUTS << 8) | (PUTS >> 8);
+            fwrite(&PUTS, sizeof(uint16_t), 1, f2);
+            
+        }
+        else if(strcmp(tokenizer[j], "HALT") == 0){
+            
+            HALT = 0xF025;
+            HALT = (HALT << 8) | (HALT >> 8);
+            fwrite(&HALT, sizeof(uint16_t), 1, f2);
+
+            fwrite(&text, sizeof(text), 1, f2);
+            fwrite(&text, sizeof(text), 1, f2);
+            
+
+        }
         
-        ADD = 0x1007;
-        ADD = (ADD << 8) | (ADD >> 8);
     }
-    if(strcmp(tokenizer[5], "HALT") == 0){
-        
-        HALT = 0xF025;
-        HALT = (HALT << 8) | (HALT >> 8);
-    }
+    
+    
 
 
     
 
-    FILE *f2 = fopen("yes.obj", "wb");
-    fwrite(&orig, sizeof(uint16_t), 1, f2);
-    fwrite(&ADD, sizeof(uint16_t), 1, f2);
-    fwrite(&HALT, sizeof(uint16_t), 1, f2);
-
+   
 
 
 
 
     fclose(file);
+    fclose(f2);
     free(tokenizer);
 
 
